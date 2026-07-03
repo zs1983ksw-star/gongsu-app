44886058c38853e7fab95a533a6589cd47639352
zs1983ksw-star <zs1983ksw@gmail.com>
2026-07-03 15:03:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+const CACHE="gongsu-trial-v15";
+const FILES=["./","./index.html","./manifest.webmanifest","./app-icon.svg","./ui-translations.js?v=5"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match("./index.html"))))});
 const CACHE="gongsu-trial-v14";
 const FILES=["./","./index.html","./manifest.webmanifest","./app-icon.svg","./ui-translations.js?v=5"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
